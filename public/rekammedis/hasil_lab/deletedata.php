1ad25c6b588f301a0a8b7fba9a397db59222a4da
raiswathan <raisulwathan07@gmail.com>
2022-05-22 23:00:09 +0700

    membuat code delete data utk hasil lab

---
+++ public/rekammedis/hasil_lab/deletedata.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../../config.php';
+
+$_SESSION['id_rm'];
+
+if (isset($_POST['deletedata'])) {
+    $query = "DELETE FROM hasil_lab WHERE id_rm='" . $_SESSION['id_rm'] . "'";
+    $query_run = mysqli_query($conn, $query);
+
+    if ($query_run) {
+        echo '<script> alert("Data Deleted"); </script>';
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    } else {
+        echo '<script> alert("Data Not Deleted"); </script>';
+    }
+}
